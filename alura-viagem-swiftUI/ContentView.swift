981f3bba54134bbe429b56ab0053b88157f18aff
Marcos Felipe Souza <mfelipesp@gmail.com>
2021-07-05 11:27:25 -0300

    Initial Commit

---
+++ alura-viagem-swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  alura-viagem-swiftUI
+//
+//  Created by Marcos Felipe Souza on 05/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ alura-viagem-swiftUI/alura_viagem_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  alura_viagem_swiftUIApp.swift
+//  alura-viagem-swiftUI
+//
+//  Created by Marcos Felipe Souza on 05/07/21.
+//
+
+import SwiftUI
+
+@main
+struct alura_viagem_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
